635ca1f8668ad369c5ace59bfeb6e0140839f506
Ross Lonstein <rlonstein@pobox.com>
2011-03-25 23:31:14 -0400

    add sysv4r checksum praxis

---
+++ sysv4r-sum.lisp
@@ -0,0 +1,18 @@
+;;;
+;;; Compute a SV4R checksum
+;;;
+;;; compare to gnu coreutils sum output
+;;;
+;;; R. Lonstein, 2011
+;;;
+(defun sv4r-sum (file)
+  "Calculate a SysV4R-style file checksum"
+  (with-open-file (stream file :direction :input :element-type 'unsigned-byte)
+    (loop :with buffer = (make-array 512 :element-type 'unsigned-byte)
+          :with csum = 0
+          :for pos = (read-sequence buffer stream)
+          :while (plusp pos)
+          :do (loop :for b :from 0 :to (1- pos)
+                    :do (setf csum (mod (+ csum (svref buffer b)) 65535)))
+          :counting buffer into c
+          :finally (return (values csum c)))))
