96b21f4323f8a5f758bb29967a1aea85ff0955c0
Ross Lonstein <rlonstein@pobox.com>
2011-04-05 14:53:07 -0400

    fix typos in sysv4r-sum

--- sysv4r-sum.lisp
+++ sysv4r-sum.lisp
@@ -6,13 +6,13 @@
 ;;; R. Lonstein, 2011
 ;;;
 (defun sv4r-sum (file)
-  "Calculate a SysV4R-style file checksum"
+  "Calculate a Unix SV4R-style file checksum"
   (with-open-file (stream file :direction :input :element-type 'unsigned-byte)
     (loop :with buffer = (make-array 512 :element-type 'unsigned-byte)
-          :with csum = 0
+          :and csum = 0
           :for pos = (read-sequence buffer stream)
           :while (plusp pos)
           :do (loop :for b :from 0 :to (1- pos)
                     :do (setf csum (mod (+ csum (svref buffer b)) 65535)))
-          :counting buffer into c
+          :counting buffer :into c
           :finally (return (values csum c)))))
