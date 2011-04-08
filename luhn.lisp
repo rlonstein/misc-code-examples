525518ffb59cf9b3318e78ca9fdc97fce24b4af5
Ross Lonstein <rlonstein@pobox.com>
2011-04-08 17:06:36 -0400

    refactor the encode/decode routines into one help that does both

--- luhn.lisp
+++ luhn.lisp
@@ -10,21 +10,23 @@
         :when digit
         :collect digit))
 
-(defun luhn-check (num)
-  (let ((luhn-double #(0 2 4 6 8 1 3 5 7 9))
-        (d (reverse (digits num))))
-    (loop :with checksum = 0
-          :for n :in d
-          :for alternate = t :then (not alternate)
-          :do (setf checksum (+ checksum (if alternate (svref luhn-double n) n)))
-          :finally (return (mod (- 10 (mod checksum 10)) 10)))))
-
-(defun luhn-p (num)
-  (the integer num)
+(defun luhn-helper (num &key (verify t))
+  "Calculate or Verify a Luhn 10 checksum"
+  ;; refactored nearly identical routines that encode/decode the luhn
+  ;; checksum into one that does both
   (let* ((luhn-double #(0 2 4 6 8 1 3 5 7 9))
          (d (reverse (digits num)))
-         (checksum (pop d)))
+         (checksum (if verify (pop d) 0)))
     (loop :for n :in d
           :for alternate = t :then (not alternate)
           :do (setf checksum (+ checksum (if alternate (svref luhn-double n) n)))
-          :finally (return (mod checksum 10)))))
+          :finally (if verify (return (mod checksum 10))
+                       (return (mod (- 10 (mod checksum 10)) 10))))))
+
+(defun luhn-check (num)
+  "Calculate a Luhn 10 checksum for a number"
+  (luhn-helper num :verify nil))
+
+(defun luhn-p (num)
+  "Verify a Luhn 10 checksummed number"
+  (luhn-helper num))
